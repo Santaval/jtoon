90e8a7b9820c497d4b53c3d2d790648c256c4102
Aaron Santana <84732450+Santaval@users.noreply.github.com>
2025-11-24 13:43:41 -0600

    feat: Integrate Microsoft Clarity for enhanced analytics tracking

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,27 @@ import ApiPage from "./pages/ApiPage";
 import WhatIsToonPage from "./pages/WhatIsToonPage";
 import { ApiDocumentationPage } from "./pages/ApiDocumentationPage";
 import { Analytics } from '@vercel/analytics/react';
+import Clarity from '@microsoft/clarity';
+import { useEffect } from "react";
 
 export default function App() {
+
+  useEffect(() => {
+    const projectId = import.meta.env.VITE_CLARITY_PROJECT_ID;
+    Clarity.init(projectId);
+  }, []);
+
   return <>
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/json-to-toon" element={<JsonToToonPage />} />
-      <Route path="/csv-to-toon" element={<CsvToToonPage />} />
-      <Route path="/api" element={<ApiPage />} />
-      <Route path="/api-docs" element={<ApiDocumentationPage />} />
-      <Route path="/what-is-toon" element={<WhatIsToonPage />} />
-    </Routes>
-  </BrowserRouter>
-  <Analytics />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/json-to-toon" element={<JsonToToonPage />} />
+        <Route path="/csv-to-toon" element={<CsvToToonPage />} />
+        <Route path="/api" element={<ApiPage />} />
+        <Route path="/api-docs" element={<ApiDocumentationPage />} />
+        <Route path="/what-is-toon" element={<WhatIsToonPage />} />
+      </Routes>
+    </BrowserRouter>
+    <Analytics />
   </>
 }
