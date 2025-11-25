8db5dacce79b93edd4771a9545fa67831b138ede
Aaron Santana <84732450+Santaval@users.noreply.github.com>
2025-11-24 23:19:11 -0600

    feat: Remove Microsoft Clarity integration from package.json and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,8 @@ import ApiPage from "./pages/ApiPage";
 import WhatIsToonPage from "./pages/WhatIsToonPage";
 import { ApiDocumentationPage } from "./pages/ApiDocumentationPage";
 import { Analytics } from '@vercel/analytics/react';
-import Clarity from '@microsoft/clarity';
-import { useEffect } from "react";
 
 export default function App() {
-
-  useEffect(() => {
-    const projectId = import.meta.env.VITE_CLARITY_PROJECT_ID;
-    Clarity.init(projectId);
-  }, []);
 
   return <>
     <BrowserRouter>
